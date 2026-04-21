d69d51c45a3df9c196138a1fcc44adbafad37727
LimDongXian <limdongx@gmail.com>
2026-04-21 21:51:29 +0800

    Get lyrics using syncedlyrics

--- detect_music.py
+++ detect_music.py
@@ -1,6 +1,16 @@
 import asyncio
-from winsdk.windows.media.control import GlobalSystemMediaTransportControlsSessionManager as MediaManager
+import syncedlyrics
+from winsdk.windows.media.control import (
+    GlobalSystemMediaTransportControlsSessionManager as MediaManager,
+)
 
+def get_synced_lyrics(query):
+    try:
+        lrc = syncedlyrics.search(query)
+        return lrc if lrc else "No lyrics found."
+
+    except Exception as e:
+        return f"Error: {e}"
 
 
 async def get_current_media():
@@ -13,6 +23,15 @@ async def get_current_media():
         title = info.title
         artist = info.artist
 
-        print(f"Now Playing: {title} by {artist}")
+        music = f"{title} by {artist}"
+        print(f"Now Playing: {music}")
+
+
+        lyrics = get_synced_lyrics(music)
+        print(lyrics)
+
+    else:
+        print("No Music is Playing")
+
 
 asyncio.run(get_current_media())
