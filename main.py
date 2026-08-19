63d787ac6bde2bf9cb24606e55bd770cb8bc1cb1
LimDongXian <limdongx@gmail.com>
2026-08-19 11:33:07 +0800

    Create necessary file that needed

---
+++ main.py
@@ -0,0 +1,20 @@
+r"""
+LyricsPlayer - Auto-synced lyrics display application.
+
+Citation for syncedlyrics library:
+@misc{syncedlyrics,
+  author = {Momeni, Mohammad},
+  title = {syncedlyrics},
+  year = {2022},
+  publisher = {GitHub},
+  journal = {GitHub repository},
+  howpublished = {\url{https://github.com/moehmeni/syncedlyrics}},
+}
+"""
+
+import asyncio
+import threading
+import tkinter as tk
+import os
+import sys
+
