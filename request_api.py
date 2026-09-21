86e4b1a13d91ca824f89cb8dc69a3fd36480237a
LimDongXian <limdongx@gmail.com>
2026-09-22 01:07:38 +0800

    Update request_api.py
    Make local the content of true, replacing the online when running this file, so modified content will be updated

--- request_api.py
+++ request_api.py
@@ -12,24 +12,29 @@ def fetch_from_my_lyrics_api(title, artist):
         response = requests.get(
             f"{MY_LYRICS_API}/songs/search",
             params={"title": title, "artist": artist},
-            timeout=3,
+            timeout=10,
         )
+
         if response.status_code == 404:
             print(f"[MyLyricsAPI] Song not found: '{title}' by '{artist}'")
             return None
-        
+
         response.raise_for_status()
+
         song = response.json()
         song_id = song["id"]
 
         lyrics_response = requests.get(
-            f"{MY_LYRICS_API}/songs/{song_id}/lyrics", timeout=3
+            f"{MY_LYRICS_API}/songs/{song_id}/lyrics",
+            timeout=10
         )
+
         if lyrics_response.status_code == 404:
             print(f"[MyLyricsAPI] Lyrics not found for song ID {song_id}")
             return None
 
         lyrics_response.raise_for_status()
+
         lyrics = lyrics_response.json()
         return lyrics
 
